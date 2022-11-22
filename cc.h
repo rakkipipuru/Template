4350524081e9464f8cea450920fc6161f2644902
Rakki Pipuru <116974720+rakkipipuru@users.noreply.github.com>
2022-11-23 00:21:08 +0700

    Create cc.h

---
+++ cc.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, b) for (int i = a; i <= b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define ll long long
+
+int main() {
+  return 0;
+}
